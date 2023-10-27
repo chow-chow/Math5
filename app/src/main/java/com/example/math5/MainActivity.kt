5156123ff17bd47ecc6eb57b5a5cb067a2738e83
chow-chow <leonardochow@outlook.com>
2023-10-26 20:25:54 -0600

    Adding spinner listener on Item Selected to display quadratic formula views

--- app/src/main/java/com/example/math5/MainActivity.kt
+++ app/src/main/java/com/example/math5/MainActivity.kt
@@ -2,7 +2,16 @@ package com.example.math5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.math5.const.Options
 import com.example.math5.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +21,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Creating an ArrayAdapter from the string-array and defining the default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.formulas_array,
+            R.layout.spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.dropdown_item)
+            binding.spinner.adapter = adapter
+        }
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(this@MainActivity,
+                    "You selected ${parent?.getItemAtPosition(position).toString()} at position $position",
+                    Toast.LENGTH_LONG).show()
+                when (position) {
+                    Options.QUADRATIC -> { displayQuadratic() }
+                    Options.SIMULTANEOUS -> { displaySimultaneous() }
+                    Options.OHMLAW -> { displayOhmLaw() }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        Log.d(TAG, "SelectedItem: ${binding.spinner.selectedItemPosition}")
+    }
+
+    private fun displayOhmLaw() {
+        TODO("Not yet implemented")
+    }
+
+    private fun displaySimultaneous() {
+        TODO("Not yet implemented")
+    }
+
+    private fun displayQuadratic() {
+        val quadraticLayout = LayoutInflater.from(this).inflate(R.layout.quadratic_layout, null)
+        binding.formula.setImageResource(R.drawable.quadratic_equationv2)
+        binding.container.removeAllViews()
+        binding.container.addView(quadraticLayout)
     }
 }
---
+++ app/src/main/java/com/example/math5/const/Options.kt
@@ -0,0 +1,7 @@
+package com.example.math5.const
+
+object Options {
+    const val QUADRATIC = 0
+    const val SIMULTANEOUS = 1
+    const val OHMLAW = 2
+}
